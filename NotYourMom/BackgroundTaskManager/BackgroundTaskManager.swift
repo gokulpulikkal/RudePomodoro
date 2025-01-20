0ad521edd16ad90736c886b1dd517a2e1ae09fde
Gokul Pulikkal <g.gokuldas68@gmail.com>
2025-01-19 23:15:53 -0500

    added music player instead of location service

--- NotYourMom/BackgroundTaskManager/BackgroundTaskManager.swift
+++ NotYourMom/BackgroundTaskManager/BackgroundTaskManager.swift
@@ -5,19 +5,41 @@
 //  Created by Gokul P on 1/16/25.
 //
 
-import CoreLocation
+import AVFoundation
 import Foundation
 import UserNotifications
 
-class BackgroundTaskManager: NSObject, CLLocationManagerDelegate {
+class BackgroundTaskManager: NSObject {
+
+    var avPlayer: AVAudioPlayer?
+    let path = Bundle.main.path(forResource: "rain.wav", ofType: nil)
+
     static let shared = BackgroundTaskManager()
-    private let locationManager = LocationManager.shared
+
+    override private init() {
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(.playback)
+        } catch {
+            print("Setting category to AVAudioSessionCategoryPlayback failed.")
+        }
+    }
 
     func startBackgroundLocationTask() {
-        locationManager.startMonitoring()
+        guard let path else {
+            return
+        }
+        let url = URL(fileURLWithPath: path)
+        do {
+            avPlayer = try AVAudioPlayer(contentsOf: url)
+            avPlayer?.numberOfLoops = -1
+            avPlayer?.play()
+        } catch {
+            print("couldn't load file :(")
+        }
     }
 
     func stopBackgroundTask() {
-        locationManager.stopMonitoring()
+        avPlayer?.stop()
     }
 }
---
+++ NotYourMom/BackgroundTaskManager/BackgroundTaskProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  BackgroundTaskProtocol.swift
+//  NotYourMom
+//
+//  Created by Gokul P on 1/19/25.
+//
+
+import Foundation
+
+protocol BackgroundTaskProtocol {
+    
+}
